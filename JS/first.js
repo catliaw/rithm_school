3bfe1e8d89ef1dc5999fb267a901571c8a0c96cb
catliaw <cat.liaw@gmail.com>
2017-03-02 14:07:00 -0800

    Adding initial JS Fundamentals files and notes.

---
+++ JS/first.js
@@ -0,0 +1,52 @@
+// alert("Coming to you from first.js!");
+
+// var one = 1;
+// var two = 2;
+// console.log("one =", one);
+// console.log("two =", two);
+
+// var surname = prompt("Please enter your name");
+// console.log("Your name is", surname);
+
+// var nameOK = confirm("Confirm that your name is " + surname + ".");
+// console.log("Your name is", surname, ":", nameOK);
+
+// var firstName = "Cat";
+// var dayOfBirth = 12;
+
+// firstName = "Catherine";
+
+// firstName = "Cathy";
+
+// var favColor = prompt("What is your favorite color?");
+// console.log(favColor, "is also my favorite color!");
+
+// var singleQuote = 'She said, "What is your favoite color?"';
+// var doubleQuote = "I said, \"Get out\'a here! We have so much in common!\"";
+// console.log(singleQuote);
+// console.log(doubleQuote);
+
+// 2 == "2"; //true
+// 2 === 2; //true
+// 10 % 3; //1
+// 10 % 3 === 1; //true
+// true && false; //false
+// false || true; //true
+// true || false; //true
+
+// randomNum = Math.random();
+// console.log(randomNum);
+
+// if (randomNum >= 0.5) {
+//     console.log("Over 0.5");
+// } else {
+//     console.log("Under 0.5");
+// }
+
+// Falsey values:
+// 0
+// undefined
+// null
+// ""
+// false
+// NaN
